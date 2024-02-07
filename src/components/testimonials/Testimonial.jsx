0ae7bb1e5613fab0a3f03383da90b54e085ed290
mian312 <deymilan312@gmail.com>
2024-02-07 19:33:18 +0530

    hidden card

--- src/components/testimonials/Testimonial.jsx
+++ src/components/testimonials/Testimonial.jsx
@@ -90,7 +90,7 @@ const Testimonial = () => {
             <div className="self-stretch mt-16 max-md:mt-10">
                 {/* For Large screens */}
                 <div
-                    className='grid lg:grid-cols-3 md:grid-cols-2 gap-4 overflow-x-hidden flex-col mt-5'
+                    className='grid lg:grid-cols-3 md:grid-cols-2 gap-4 overflow-x-hidden flex-col mt-5 max-sm:hidden'
                 >
                     {testimonials.map((testimonial, index) => (
                         <div key={index} className='hover:scale-95 cursor-pointer'>
