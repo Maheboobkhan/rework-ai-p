61d0e4368cda2d77f99faf82abcfbaa45a9ceb23
mian312 <deymilan312@gmail.com>
2024-02-08 10:07:22 +0530

    did footer as figma

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
           <div className="text-3xl font-extrabold whitespace-nowrap">
             <span className="grow font-Raleway text-teal-600">Talent </span>
             <span className="grow font-Raleway sm:text-white text-opacity-90">Finder</span>
-            <p className='font-Caveat text-xs text-end mr-16 max-sm:mr-4 font-[14px] max-sm:font-[9.11px] sm:text-white italic text-[C7C7C7] w-[101.42] h-[23.7]'>by rework</p>
+            <p className='font-Caveat text-xs text-end mr-4 font-[14px] max-sm:font-[9.11px] sm:text-white italic text-[C7C7C7] w-[101.42] h-[23.7]'>by rework</p>
           </div>
 
           <p className="font-Roboto font-[15.38px] max-sm:font-[12px] mt-8 text-base leading-6 text-white px-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</p>
@@ -47,11 +47,11 @@ const Footer = () => {
             <AccordianFeature />
           </div>
           {/* NEWS LETTER */}
-          <div className="flex flex-col self-end px-px mt-2 whitespace-nowrap">
-            <div className="font-Inter self-start ml-6 text-lg font-bold leading-7 text-white max-md:ml-2.5">
+          <div className="flex flex-col self-end px-px mt-7 whitespace-nowrap">
+            <div className="font-Inter self-start text-lg font-bold leading-7 text-white">
               Newsletter
             </div>
-            <form onSubmit={handleSubmit} className="flex justify-between mt-7 text-base leading-6">
+            <form onSubmit={handleSubmit} className="flex justify-between text-base leading-6 mt-2">
               <input
                 type="email"
                 className="justify-center px-3.5 py-3 bg-white rounded-lg border border-solid shadow-sm text-neutral-500"
