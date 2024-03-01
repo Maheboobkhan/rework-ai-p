ce1c7f71e1d2527b80dec60b2dfc5c7179341b24
mian312 <deymilan312@gmail.com>
2024-03-01 16:26:51 +0530

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import "./App.css";
 import Home from './pages/Home'
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router";
-<<<<<<< HEAD
 import SignUp from "./pages/SignUp";
-=======
 import ViewAllProfileCard from "./pages/ViewAllProfileCard";
->>>>>>> 4070e6f59e9575a8e396995a73915f21a85acc2f
 
 function App() {
 
@@ -16,11 +13,8 @@ function App() {
     path: "/",
     children: [
       { path: "/", element: <Home /> },
-<<<<<<< HEAD
       { path: "/signup", element: <SignUp /> },
-=======
       { path: "/view-all-profile-cards", element: <ViewAllProfileCard /> },
->>>>>>> 4070e6f59e9575a8e396995a73915f21a85acc2f
     ],
   }])
 
